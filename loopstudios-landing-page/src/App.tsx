c2e4f2bc31caf22fc8b5cef823a466ad7622e19c
Alex Ding <Alex.Ding.builds@gmail.com>
2026-04-17 22:25:34 +0800

    finished

--- loopstudios-landing-page/src/App.tsx
+++ loopstudios-landing-page/src/App.tsx
@@ -1,4 +1,29 @@
 import {useState,type Dispatch,type SetStateAction} from "react";
+import logo from '../public/images/logo.svg'
+import hamburger from '../public/images/icon-hamburger.svg'
+import close from '../public/images/icon-close.svg'
+import minteractive from '../public/images/mobile/image-interactive.jpg'
+import dinteractive from '../public/images/desktop/image-interactive.jpg'
+import mcuriosity from '../public/images/mobile/image-curiosity.jpg'
+import dcuriosity from '../public/images/desktop/image-curiosity.jpg'
+import mdeep from '../public/images/mobile/image-deep-earth.jpg'
+import ddeep from '../public/images/desktop/image-deep-earth.jpg'
+import mnightArcade from '../public/images/mobile/image-night-arcade.jpg'
+import dnightArcade from '../public/images/desktop/image-night-arcade.jpg'
+import msoccerTeam from '../public/images/mobile/image-soccer-team.jpg'
+import dsoccerTeam from '../public/images/desktop/image-soccer-team.jpg'
+import mgrid from '../public/images/mobile/image-grid.jpg'
+import dgrid from '../public/images/desktop/image-grid.jpg'
+import mfromAbove from '../public/images/mobile/image-from-above.jpg'
+import dfromAbove from '../public/images/desktop/image-from-above.jpg'
+import mpocketBorealis from '../public/images/mobile/image-pocket-borealis.jpg'
+import dpocketBorealis from '../public/images/desktop/image-pocket-borealis.jpg'
+import mfisheye from '../public/images/mobile/image-fisheye.jpg'
+import dfisheye from '../public/images/desktop/image-fisheye.jpg'
+import facebook from '../public/images/icon-facebook.svg'
+import twitter from '../public/images/icon-twitter.svg'
+import pinterest from '../public/images/icon-pinterest.svg'
+import instagram from '../public/images/icon-instagram.svg'
 
 export default function App() {
   const [isMenuOpened , setIsMenuOpened]: [boolean, Dispatch<SetStateAction<boolean>>] = useState(false)
@@ -14,10 +39,10 @@ export default function App() {
         <div className="max-w-[608px] lg:max-w-[1110px] lg:w-full">
           <div className="flex justify-between items-center mb-[clamp(163px,_-8.3969vw_+_194.4885px,_130px)]
           lg:mb-[127px]">
-            <img src="/images/logo.svg" alt="Loopstudios"
+            <img src={logo} alt="Loopstudios"
             className="w-[clamp(144px,_12.2137vw_+_98.1985px,_192px)]" />
             <button type="button" aria-label="Open menu" className="lg:hidden" onClick={handleClick}>
-              <img src="/images/icon-hamburger.svg" alt="Menu"
+              <img src={hamburger} alt="Menu"
               className="w-[clamp(24px,_2.0356vw_+_16.3664px,_32px)]" />
             </button>
             <nav className="hidden lg:block">
@@ -39,9 +64,9 @@ export default function App() {
      justify-center">
         <div className="md:w-[607.51px] h-full flex flex-col justify-between w-full">
           <div className="flex justify-between items-center">
-            <img src="/images/logo.svg" alt="Loopstudios"
+            <img src={logo} alt="Loopstudios"
                 className="w-[clamp(144px,_12.2137vw_+_98.1985px,_192px)]" />
-            <img src="/images/icon-close.svg" alt="close menu" className="hover:cursor-pointer hover:opacity-70 hover:transition-opacity" onClick={handleClick} />
+            <img src={close} alt="close menu" className="hover:cursor-pointer hover:opacity-70 hover:transition-opacity" onClick={handleClick} />
           </div>
           <nav>
             <ul className="flex text-white text-preset-5 uppercase gap-6 flex-col">
@@ -58,8 +83,8 @@ export default function App() {
       lg:pt-[160px] lg:pb-[182px] lg:px-[160px]">
         <section className="mb-24 max-w-[608px] lg:max-w-[1111px] lg:flex lg:items-end lg:mb-[184px]">
           <picture>
-            <source media="(min-width: 768px)" srcSet="/images/desktop/image-interactive.jpg" />
-            <img src="/images/mobile/image-interactive.jpg" alt="Interactive Design"
+            <source media="(min-width: 768px)" srcSet={dinteractive} />
+            <img src={minteractive} alt="Interactive Design"
             className="w-[clamp(327px,_71.5013vw_+_58.8702px,_608px)] mb-12
             lg:mb-0 lg:w-[731px]" />
           </picture>
@@ -79,64 +104,64 @@ export default function App() {
           <div className="grid gap-6 mb-8 w-full
           lg:grid-cols-4">
             <picture className="relative">
-              <source media="(min-width: 1440px)" srcSet="/images/desktop/image-deep-earth.jpg" />
-              <img src="/images/mobile/image-deep-earth.jpg" alt="Deep Earth"
+              <source media="(min-width: 1440px)" srcSet={ddeep} />
+              <img src={mdeep} alt="Deep Earth"
               className="h-[120px] object-cover w-full object-center
               lg:h-[450px] lg:w-full" />
               <div className="w-full h-full absolute bg-linear-to-r from-black/60 to-black/0 top-0 pb-[20px] text-white flex items-end justify-start pl-[clamp(20px,_4.374vw_+_3.5973px,_37.19px)]
               text-preset-5 uppercase lg:bg-linear-to-t lg:text-preset-4 lg:pb-8 lg:pl-10">deep <br /> earth</div>
             </picture>
             <picture className="relative">
-              <source media="(min-width: 1440px)" srcSet="/images/desktop/image-night-arcade.jpg" />
-              <img src="/images/mobile/image-night-arcade.jpg" alt="Night Arcade"
+              <source media="(min-width: 1440px)" srcSet={dnightArcade} />
+              <img src={mnightArcade} alt="Night Arcade"
               className="h-[120px] object-cover w-full object-center
               lg:h-[450px] lg:w-full" />
               <div className="w-full h-full absolute bg-linear-to-r from-black/60 to-black/0 top-0 pb-[20px] text-white flex items-end justify-start pl-[clamp(20px,_4.374vw_+_3.5973px,_37.19px)]
               text-preset-5 uppercase lg:bg-linear-to-t lg:text-preset-4 lg:pb-8 lg:pl-10">night <br /> arcade</div>
             </picture>
             <picture className="relative">
-              <source media="(min-width: 1440px)" srcSet="/images/desktop/image-soccer-team.jpg" />
-              <img src="/images/mobile/image-soccer-team.jpg" alt="Soccer Team"
+              <source media="(min-width: 1440px)" srcSet={dsoccerTeam} />
+              <img src={msoccerTeam} alt="Soccer Team"
               className="h-[120px] object-cover w-full object-top
               lg:h-[450px] lg:w-full" />
               <div className="w-full h-full absolute bg-linear-to-r from-black/60 to-black/0 top-0 pb-[20px] text-white flex items-end justify-start pl-[clamp(20px,_4.374vw_+_3.5973px,_37.19px)]
               text-preset-5 uppercase lg:bg-linear-to-t lg:text-preset-4 lg:pb-8 lg:pl-10">soccer <br /> team vr</div>
             </picture>
             <picture className="relative">
-              <source media="(min-width: 1440px)" srcSet="/images/desktop/image-grid.jpg" />
-              <img src="/images/mobile/image-grid.jpg" alt="Grid"
+              <source media="(min-width: 1440px)" srcSet={dgrid} />
+              <img src={mgrid} alt="Grid"
               className="h-[120px] object-cover w-full object-center
               lg:h-[450px] lg:w-full" />
               <div className="w-full h-full absolute bg-linear-to-r from-black/60 to-black/0 top-0 pb-[20px] text-white flex items-end justify-start pl-[clamp(20px,_4.374vw_+_3.5973px,_37.19px)]
               text-preset-5 uppercase lg:bg-linear-to-t lg:text-preset-4 lg:pb-8 lg:pl-10">the <br />grid</div>
             </picture>
             <picture className="relative">
-              <source media="(min-width: 1440px)" srcSet="/images/desktop/image-from-above.jpg" />
-              <img src="/images/mobile/image-from-above.jpg" alt="From Above"
+              <source media="(min-width: 1440px)" srcSet={dfromAbove} />
+              <img src={mfromAbove} alt="From Above"
               className="h-[120px] object-cover w-full object-bottom
               lg:h-[450px] lg:w-full" />
               <div className="w-full h-full absolute bg-linear-to-r from-black/60 to-black/0 top-0 pb-[20px] text-white flex items-end justify-start pl-[clamp(20px,_4.374vw_+_3.5973px,_37.19px)]
               text-preset-5 uppercase lg:bg-linear-to-t lg:text-preset-4 lg:pb-8 lg:pl-10">from up <br /> above vr</div>
             </picture>
             <picture className="relative">
-              <source media="(min-width: 1440px)" srcSet="/images/desktop/image-pocket-borealis.jpg" />
-              <img src="/images/mobile/image-pocket-borealis.jpg" alt="Pocket Borealis"
+              <source media="(min-width: 1440px)" srcSet={dpocketBorealis} />
+              <img src={mpocketBorealis} alt="Pocket Borealis"
               className="h-[120px] object-cover w-full object-center
               lg:h-[450px] lg:w-full" />
               <div className="w-full h-full absolute bg-linear-to-r from-black/60 to-black/0 top-0 pb-[20px] text-white flex items-end justify-start pl-[clamp(20px,_4.374vw_+_3.5973px,_37.19px)]
               text-preset-5 uppercase lg:bg-linear-to-t lg:text-preset-4 lg:pb-8 lg:pl-10">pocket  <br /> borealis</div>
             </picture>
             <picture className="relative">
-              <source media="(min-width: 1440px)" srcSet="/images/desktop/image-curiosity.jpg" />
-              <img src="/images/mobile/image-curiosity.jpg" alt="Curiosity"
+              <source media="(min-width: 1440px)" srcSet={dcuriosity} />
+              <img src={mcuriosity} alt="Curiosity"
               className="h-[120px] object-cover w-full object-bottom
               lg:h-[450px] lg:w-full" />
               <div className="w-full h-full absolute bg-linear-to-r from-black/60 to-black/0 top-0 pb-[20px] text-white flex items-end justify-start pl-[clamp(20px,_4.374vw_+_3.5973px,_37.19px)]
               text-preset-5 uppercase lg:bg-linear-to-t lg:text-preset-4 lg:pb-8 lg:pl-10">the <br /> curiosity</div>
             </picture>
             <picture className="relative">
-              <source media="(min-width: 1440px)" srcSet="/images/desktop/image-fisheye.jpg" />
-              <img src="/images/mobile/image-fisheye.jpg" alt="Fisheye"
+              <source media="(min-width: 1440px)" srcSet={dfisheye} />
+              <img src={mfisheye} alt="Fisheye"
               className="h-[120px] object-cover w-full object-top
               lg:h-[450px] lg:w-full" />
               <div className="w-full h-full absolute bg-linear-to-r from-black/60 to-black/0 top-0 pb-[20px] text-white flex items-end justify-start pl-[clamp(20px,_4.374vw_+_3.5973px,_37.19px)]
@@ -152,7 +177,7 @@ export default function App() {
         <div className="max-w-[263px] flex flex-col items-center w-full
         md:max-w-[608px] lg:max-w-[1100px]">
           <div className="mb-4 w-full flex flex-col items-center md:flex-row md:justify-between md:mb-6">
-              <img src="/images/logo.svg" alt="Loopstudios"
+              <img src={logo} alt="Loopstudios"
               className="w-[144px]" />
               <nav className="md:hidden">
                 <ul className="flex flex-col items-center my-10 text-preset-6 gap-4
@@ -165,10 +190,10 @@ export default function App() {
                 </ul>
               </nav>
             <div className="flex gap-4">
-              <a href="https://facebook.com" aria-label="Facebook"><img src="/images/icon-facebook.svg" alt="Facebook" /></a>
-              <a href="https://twitter.com" aria-label="Twitter"><img src="/images/icon-twitter.svg" alt="Twitter" /></a>
-              <a href="https://pinterest.com" aria-label="Pinterest"><img src="/images/icon-pinterest.svg" alt="Pinterest" /></a>
-              <a href="https://instagram.com" aria-label="Instagram"><img src="/images/icon-instagram.svg" alt="Instagram" /></a>
+              <a href="https://facebook.com" aria-label="Facebook"><img src={facebook} alt="Facebook" /></a>
+              <a href="https://twitter.com" aria-label="Twitter"><img src={twitter} alt="Twitter" /></a>
+              <a href="https://pinterest.com" aria-label="Pinterest"><img src={pinterest} alt="Pinterest" /></a>
+              <a href="https://instagram.com" aria-label="Instagram"><img src={instagram} alt="Instagram" /></a>
             </div>
           </div>
           <div className="md:flex md:justify-between md:w-full text-preset-6 ">
